664cf3bf579e7fac7798f1bc3116055db40fe663
ttr3dp <git@ttr3dp.net>
2021-01-10 02:55:44 +0100

    Add alpha to Xresources

--- config.h
+++ config.h
@@ -223,6 +223,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",        FLOAT,   &alpha },
 };
 
 /*
