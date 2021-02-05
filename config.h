0422baf401e78b4398841a529e11c4d1c509d55d
ttr3dp <git@ttr3dp.net>
2021-02-05 02:28:09 +0100

    Reset chscale

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.2;
+static float chscale = 1.0;
 
 /*
  * word delimiter string
